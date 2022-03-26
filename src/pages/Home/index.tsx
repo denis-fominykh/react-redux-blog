52af014040c849cefafe0e2c1b715590ef2ddd7e
denisfominykh <2028902d>
2022-03-27 01:39:31 +0200

    fix: change a home page content

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,36 +1,11 @@
-import { useEffect } from 'react';
-
-import logo from 'assets/images/svg/react-router-logo.svg';
-import {
-  HomeContainer,
-  HomeLogoContainer,
-  HomeHeader,
-  HomeCode,
-  HomeLink,
-  HomeLogo,
-} from 'pages/Home/styled';
-import { useGetPeopleData } from 'redux/people/hooks';
-
-export function Home() {
-  const getPeopleData = useGetPeopleData();
-
-  useEffect(() => {
-    getPeopleData('1');
-  }, []);
-
+export function Home(): JSX.Element {
   return (
-    <HomeContainer>
-      <HomeHeader>
-        <HomeLogoContainer>
-          <HomeLogo src={logo} alt="logo" />
-        </HomeLogoContainer>
-        <p>
-          Edit <HomeCode>src/App.tsx</HomeCode> and save to reload.
-        </p>
-        <HomeLink href="https://reactrouter.com" target="_blank" rel="noopener noreferrer">
-          Learn React Router
-        </HomeLink>
-      </HomeHeader>
-    </HomeContainer>
+    <div>
+      <h1>Welcome!</h1>
+      <p>
+        React.JS is a front-end web library by Meta. This is a simple blog app to demonstrate the
+        power of React.JS.
+      </p>
+    </div>
   );
 }
