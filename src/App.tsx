be4a30d43af87ca2637500fd65c52f745fb5bbec
denisfominykh <2028902d>
2022-03-27 01:46:31 +0200

    feat: connect react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import { Home } from 'pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-export function App() {
+import { NavBar } from 'components/NavBar';
+import { Home } from 'pages/Home';
+import { People } from 'pages/People';
+import { PeopleItem } from 'pages/People/PeopleItem';
+
+export function App(): JSX.Element {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <NavBar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/people/:itemId" element={<PeopleItem />} />
+        </Routes>
+      </NavBar>
+    </BrowserRouter>
   );
 }
