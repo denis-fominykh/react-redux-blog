f02b54a2f9274112c40579ba7a42fef6bc83d19e
denisfominykh <2028902d>
2022-03-27 02:02:28 +0200

    feat: create a page with a specific character by id from sw

---
+++ src/pages/People/PeopleItem/index.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import { Loader } from 'components/Loader';
+import { PageHeader, PageContent, ButtonReverseLink } from 'pages/People/PeopleItem/styled';
+import { useGetPeopleItemData } from 'redux/people/hooks';
+import { selectPeopleItemData, selectPeopleItemLoader } from 'redux/people/selectors';
+
+type PeopleParams = {
+  itemId: string | undefined;
+};
+
+export function PeopleItem(): JSX.Element {
+  const { itemId } = useParams<PeopleParams>();
+  const getPeopleData = useGetPeopleItemData();
+  const { name, gender, mass, height, birthYear } = useSelector(selectPeopleItemData, shallowEqual);
+  const loader = useSelector(selectPeopleItemLoader, shallowEqual);
+
+  useEffect(() => {
+    if (itemId) {
+      getPeopleData(itemId);
+    }
+  }, []);
+
+  return (
+    <div>
+      {loader ? (
+        <Loader />
+      ) : (
+        <>
+          <PageHeader>
+            <h1>{name}</h1>
+            <ButtonReverseLink to="/people">Back</ButtonReverseLink>
+          </PageHeader>
+          <PageContent>
+            <p>Gender: {gender}</p>
+            <p>Mass: {mass}</p>
+            <p>Height: {height}</p>
+            <p>Birth year: {birthYear}</p>
+          </PageContent>
+        </>
+      )}
+    </div>
+  );
+}
