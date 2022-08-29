f2428c024e91894955790f0b99bbeacaf5e2d4a1
denis_fominykh <dfominykh@s-pro.io>
2022-08-29 21:19:16 +0300

    fix: change library

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 
 import { createReducer } from 'redux/rootReducer';
